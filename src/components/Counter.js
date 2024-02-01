2c19f86c10776521299390850e215bc8d01f91a3
A-phenomenal1 <niteshch200053@gmail.com>
2024-02-01 20:02:02 +0530

    multiple reducers

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,32 +1,66 @@
 import React, { useReducer } from "react";
 
-const initialState = 0;
+const initialState = {
+  count: 0,
+};
+const initialState2 = {
+  count: 0,
+};
 
-const reducer = (state, action) => {
-  switch (action) {
+const countReducer = (state, action) => {
+  switch (action.type) {
     case "increament":
-      return state + 1;
+      return { ...state, count: state.count + 1 };
     case "decreament":
-      return state - 1;
+      return { ...state, count: state.count - 1 };
+    case "increament5":
+      return { ...state, count: state.count + 5 };
+    case "decreament5":
+      return { ...state, count: state.count - 5 };
     case "reset":
       return initialState;
     default:
       return state;
   }
 };
+const count2Reducer = (state, action) => {
+  switch (action.type) {
+    case "increament10":
+      return { ...state, count: state.count + 10 };
+    case "decreament10":
+      return { ...state, count: state.count - 10 };
+    default:
+      return state;
+  }
+};
 
 function Counter() {
-  const [count, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(countReducer, initialState);
+  const [state2, dispatch2] = useReducer(count2Reducer, initialState2);
   return (
-    <div style={{ display: "flex", alignItems: "center", gap: 10, flexDirection: "column" }}>
-      <h1>Reducer - {count}</h1>
-      <h3>Actions - </h3>
-      <div style={{ display: "flex", gap: 10 }}>
-        <button onClick={() => dispatch("increament")}>Increase</button>
-        <button onClick={() => dispatch("decreament")}>Decrease</button>
-        <button onClick={() => dispatch("reset")}>Reset</button>
+    <>
+      <div style={{ display: "flex", alignItems: "center", gap: 10, flexDirection: "column" }}>
+        <h1>Reducer - {state.count}</h1>
+        <h3>Actions - </h3>
+        <div style={{ display: "flex", gap: 10 }}>
+          <button onClick={() => dispatch({ type: "increament" })}>Increase</button>
+          <button onClick={() => dispatch({ type: "decreament" })}>Decrease</button>
+          <br />
+          <button onClick={() => dispatch({ type: "increament5" })}>Increase 5</button>
+          <button onClick={() => dispatch({ type: "decreament5" })}>Decrease 5</button>
+          <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
+          <br />
+        </div>
       </div>
-    </div>
+      <div style={{ display: "flex", alignItems: "center", gap: 10, flexDirection: "column" }}>
+        <h1>Reducer2 - {state2.count}</h1>
+        <h3>Actions2 - </h3>
+        <div style={{ display: "flex", gap: 10 }}>
+          <button onClick={() => dispatch2({ type: "increament10" })}>Increase 10</button>
+          <button onClick={() => dispatch2({ type: "decreament10" })}>Decrease 10</button>
+        </div>
+      </div>
+    </>
   );
 }
 
